fccb50aae13ffcb51ccf194e4892ce453ac3c472
CzarAugustus <caugustosl@yahoo.com.br>
2024-11-05 20:49:44 -0300

    sincronizando os exercícios 1011 e 1012 da beecrowd

---
+++ 1012.py
@@ -0,0 +1,10 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+areaTriangulo = (A * C)/3
+areaCirculo = (C ** 2) * 3.14159
+areaQuadrado = B ** 2
+areaRetangulo = A * B
+
+print(f"TRIANGULO = {areaTriangulo:.3f}")
